42a65820c05da4d3892ff21accfc0641658bc3f7
qaisirnaseer302 <qaisirnaseer302@gmail.com>
2026-02-14 03:10:30 +0500

    deleted files

--- controllers/rountingControllers/homeController.js
+++
@@ -1,8 +0,0 @@
-const views=require('../../path/paths')
-const homeController=(req,res)=>{;
-    res.sendFile(`${views}/index.html`)
-}
-const recipeController=(req,res)=>{
-    res.sendFile(`${views}/recipes.html`)
-}
-module.exports={homeController,recipeController};
--- routes/homeRoute.js
+++
@@ -1,6 +0,0 @@
-const express=require('express');
-const route=express.Router();
-const {homeController,recipeController}=require('../controllers/rountingControllers/homeController')
-route.get('/',homeController);
-route.get('/recipeDetails',recipeController);
-module.exports=route;
