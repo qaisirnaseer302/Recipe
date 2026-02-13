81075a0a37a892ea6392ab21d174f1aeb57c1e1d
qaisirnaseer302 <qaisirnaseer302@gmail.com>
2026-02-14 03:05:40 +0500

    user controllers to handle user routes

---
+++ controllers/rountingControllers/userControllers.js
@@ -0,0 +1,23 @@
+const {userViews}=require('../../path/paths')
+const homeController=(req,res)=>{;
+    res.sendFile(`${userViews}/index.html`)
+}
+const recipeController=(req,res)=>{
+    res.sendFile(`${userViews}/recipes.html`)
+}
+const userProofileController=(req,res)=>{
+    res.sendFile(`${userViews}/userprofile.html`)
+}
+const signUpController=(req,res)=>{
+    res.sendFile(`${userViews}/signUp.html`)
+}
+const logInController=(req,res)=>{
+    res.sendFile(`${userViews}/logIn.html`)
+}
+const wellcomeController=(req,res)=>{
+    res.sendFile(`${userViews}/wellcomePage.html`)
+}
+const editProfileController=(req,res)=>{
+    res.sendFile(`${userViews}/editprofile.html`)
+}
+module.exports={homeController,recipeController,userProofileController,signUpController,logInController,wellcomeController,editProfileController};
