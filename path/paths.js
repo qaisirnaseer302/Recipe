1a0cd582667041a8c1cff4d7c18b68f3547c3c9b
qaisirnaseer302 <qaisirnaseer302@gmail.com>
2026-02-14 03:04:55 +0500

    modified path folder to set paths

--- path/paths.js
+++ path/paths.js
@@ -1,3 +1,4 @@
 const path=require('path');
-const views=path.join(__dirname,'..','public','views');
-module.exports=views;
+const userViews=path.join(__dirname,'..','public','views','userViews');
+const adminViews=path.join(__dirname,'..','public','views','adminViews');
+module.exports={userViews,adminViews};
