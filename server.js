90d2a3a9913f0a5954ad54e997f6b5329e21c4cd
qaisirnaseer302 <qaisirnaseer302@gmail.com>
2026-02-16 01:30:52 +0500

    server updated to handle admin routes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express=require('express');
 const path=require('path');
 const app=express();
-const router=require('./routes/userRoutes')
+const userRouter=require('./routes/userRoutes');
+const adminRouter=require('./routes/adminRoutes');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
-app.use('/user',router);
+app.use('/user',userRouter);
+app.use('/admin',adminRouter)
 app.listen(3000,()=>{
     console.info(`server http://localhost:3000`);
 });
