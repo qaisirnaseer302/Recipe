ff6a17a92ac21bb3ec64d6f4363696c63d98154c
qaisirnaseer302 <qaisirnaseer302@gmail.com>
2026-02-16 01:28:11 +0500

    update the user Controllers

--- controllers/rountingControllers/userControllers.js
+++ controllers/rountingControllers/userControllers.js
@@ -1,23 +1,23 @@
 const {userViews}=require('../../path/paths')
-const homeController=(req,res)=>{;
+const homeController=(req,res)=>{
     res.sendFile(`${userViews}/index.html`)
-}
+};
 const recipeController=(req,res)=>{
     res.sendFile(`${userViews}/recipes.html`)
-}
+};
 const userProofileController=(req,res)=>{
     res.sendFile(`${userViews}/userprofile.html`)
-}
+};
 const signUpController=(req,res)=>{
     res.sendFile(`${userViews}/signUp.html`)
-}
+};
 const logInController=(req,res)=>{
     res.sendFile(`${userViews}/logIn.html`)
-}
-const wellcomeController=(req,res)=>{
-    res.sendFile(`${userViews}/wellcomePage.html`)
-}
+};
 const editProfileController=(req,res)=>{
     res.sendFile(`${userViews}/editprofile.html`)
-}
-module.exports={homeController,recipeController,userProofileController,signUpController,logInController,wellcomeController,editProfileController};
+};
+const allRecipesController=(req,res)=>{
+    res.sendFile(`${userViews}/allRecipes.html`);
+};
+module.exports={homeController,recipeController,userProofileController,signUpController,logInController,editProfileController,allRecipesController};
