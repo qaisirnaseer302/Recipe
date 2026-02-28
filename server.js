5bf5747c613a71e1f2ef4587a27d33140a091752
qaisirnaseer302 <qaisirnaseer302@gmail.com>
2026-02-28 20:51:37 +0500

    updated to handle signup and login apis

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const path=require('path');
 const app=express();
 const userRouter=require('./routes/userRoutes');
 const adminRouter=require('./routes/adminRoutes');
+const connectDB=require('./config/database');
+const userApiRoutes=require('./routes/userApiRoutes');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
+connectDB();
 app.use('/user',userRouter);
-app.use('/admin',adminRouter)
+app.use('/admin',adminRouter);
+app.use('/api/user',userApiRoutes);
 app.listen(3000,()=>{
     console.info(`server http://localhost:3000`);
 });
