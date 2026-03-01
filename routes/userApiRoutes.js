8cca0ee4d08844ba882ef6947c29f48a541c3112
qaisirnaseer302 <qaisirnaseer302@gmail.com>
2026-03-01 21:26:09 +0500

    updated

--- routes/userApiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const signUpController=require('../controllers/api/userApis/signUp');
-const loginController=require('../controllers/api/userApis/login');
-router.post('/signup',signUpController);
-router.post('/login',loginController);
-module.exports=router;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express=require('express');
 const path=require('path');
 const app=express();
-const userRouter=require('./routes/userRoutes');
-const adminRouter=require('./routes/adminRoutes');
+const userRouter=require('./routes/userRoutes/userRoutes');
+const adminRouter=require('./routes/adminRoutes/adminRoutes');
 const connectDB=require('./config/database');
-const userApiRoutes=require('./routes/userApiRoutes');
+const userApiRoutes=require('./routes/userRoutes/userApiRoutes');
+const adminApiRoutes=require('./routes/adminRoutes/adminApiRoutes');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
@@ -12,6 +13,7 @@ connectDB();
 app.use('/user',userRouter);
 app.use('/admin',adminRouter);
 app.use('/api/user',userApiRoutes);
+app.use('/api/admin',adminApiRoutes);
 app.listen(3000,()=>{
     console.info(`server http://localhost:3000`);
 });
