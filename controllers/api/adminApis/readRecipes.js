d9b119c4900ad528f3fc7c4da761324de0032da9
qaisirnaseer302 <qaisirnaseer302@gmail.com>
2026-03-11 22:28:47 +0500

    api updated to read multiple values

--- controllers/api/adminApis/readRecipes.js
+++ controllers/api/adminApis/readRecipes.js
@@ -1,7 +1,17 @@
-const readAllRecipes=(req,res)=>{
-    res.status(200).json({
-        status:true,
-        message:"All recipes retrieved successfully",
+const Recipe=require("../../../models/Recipe");
+const readAllRecipes=async(req,res)=>{
+    try{
+        const recipes=await Recipe.find();
+        res.status(200).json({
+            status:true,
+            message:"Recipes retrieved successfully",
+            data:recipes
+        });
+    }catch
+    (error){
+    res.status(500).json({
+        status:false,
+        message:"Error occurred while retrieving recipes",
         data:[]
-    });
+    });}
 }
